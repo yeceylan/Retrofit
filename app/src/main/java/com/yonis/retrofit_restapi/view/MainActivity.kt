2bf1cecd62280a66bbbe180824b2ab40c0002a7b
Yunus Emre Ceylan <yec2442@gmail.com>
2023-05-09 14:57:44 +0300

    finished

--- app/src/main/java/com/yonis/retrofit_restapi/view/MainActivity.kt
+++ app/src/main/java/com/yonis/retrofit_restapi/view/MainActivity.kt
@@ -2,22 +2,31 @@ package com.yonis.retrofit_restapi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yonis.retrofit_restapi.R
+import com.yonis.retrofit_restapi.adapter.RecyclerViewAdapter
 import com.yonis.retrofit_restapi.modal.CyrptoModal
 import com.yonis.retrofit_restapi.service.CyrptoAPI
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),RecyclerViewAdapter.Listener {
     private val BaseURL="https://raw.githubusercontent.com/"
     private var CyrptoModals:ArrayList<CyrptoModal>?=null
+    private var recyclerViewAdapter : RecyclerViewAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layoutManager :RecyclerView.LayoutManager=LinearLayoutManager(this)
+        recyclerView.layoutManager=layoutManager
 
         loadData()
     }
@@ -39,11 +48,11 @@ class MainActivity : AppCompatActivity() {
                 if(response.isSuccessful){
                     response.body()?.let {
                         CyrptoModals = ArrayList(it)
-
-                        for(cyrptoModel : CyrptoModal in CyrptoModals!!){
-                            println(cyrptoModel.currency)
-                            println(cyrptoModel.price)
+                        CyrptoModals?.let {
+                            recyclerViewAdapter=RecyclerViewAdapter(it,this@MainActivity)
+                            recyclerView.adapter=recyclerViewAdapter
                         }
+
                      }
                 }
             }
@@ -53,5 +62,9 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    override fun onItemClick(cryptoModel: CyrptoModal) {
+        Toast.makeText(this,"Clicked:${cryptoModel.currency}",Toast.LENGTH_LONG).show()
     }
 }
